63c29cefbd649a6a21b89d0aaf238f1ae032199c
Marcus Wörner <61889355+woernerm@users.noreply.github.com>
2023-02-26 23:24:48 +0100

    Add error reporting and preferred locale

--- src/fluent_anvil.js
+++ src/fluent_anvil.js
@@ -1,44 +1,68 @@
 import { Localization, DOMLocalization } from "@fluent/dom";
 import { FluentBundle, FluentResource } from "@fluent/bundle";
+import { getUserLocales } from 'get-user-locale';
 
-async function fetchResource(locale, resourceId) {
+
+async function fetch_resource(locale, resourceId) {
+    locale = locale.replace("-", "_") // Make Anvil-Compatible path
     const url = resourceId.replace("{locale}", locale);
     const response = await fetch(url);
     const text = await response.text();
     return new FluentResource(text);
   }
 
-async function createBundle(locale, resourceId) {
-    let resource = await fetchResource(locale, resourceId);
+async function create_bundle(locale, resourceId, errors) {
+    let resource = await fetch_resource(locale, resourceId);
     let bundle = new FluentBundle(locale);
-    let errors = bundle.addResource(resource);
-    if (errors.length) {
-      // Syntax errors are per-message and don't break the whole resource
+    errors.entries += bundle.addResource(resource);
+
+    // Output errors, if any.
+    for (const entry of errors.entries) {
+      console.log("Error creating bundle for locale " + locale + entry)
     }
     return bundle;
 }
 
-function create_bundle_generator(locale, fallback_locales){
-    return async function* generateBundles(resourceIds) {
-        yield await createBundle(locale, resourceIds[0]);
+function create_bundle_generator(locale, fallback_locales, errors){
+    return async function* generate_bundles(resourceIds) {
+        yield await create_bundle(locale, resourceIds[0], errors);
       
         for (const entry of fallback_locales) {
-          yield await createBundle(entry, resourceIds[0]);
+          yield await create_bundle(entry, resourceIds[0], errors);
         }
     }
+} 
+
+export function get_user_locales(fallback){
+  if (fallback){
+    return getUserLocales({fallbackLocale: fallback, useFallbackLocale: true})
+  }
+  return getUserLocales()  
 }
 
 export function init_localization(resource_path_template, locale, fallback_locales){
-    const bundle_generator = create_bundle_generator(locale, fallback_locales)
+  let dom_errors = {entries: []}
+  let main_errors = {entries: []}  
+  
+  const dom_bundle_gen = create_bundle_generator(locale, fallback_locales, dom_errors)
+  const loc_bundle_gen = create_bundle_generator(locale, fallback_locales, main_errors)
     
-    // Activate DOM localization
-    const l10n = new DOMLocalization(
-        [resource_path_template], 
-        bundle_generator
-    );
-    l10n.connectRoot(document.documentElement);
-    l10n.translateRoots();
+    
+  // Activate DOM localization
+  const dom = new DOMLocalization(
+      [resource_path_template], 
+      dom_bundle_gen
+  );
+  dom.connectRoot(document.documentElement);
+  dom.translateRoots(); 
 
-    return [l10n, new Localization([resource_path_template], bundle_generator)]
+  const main = new Localization([resource_path_template], loc_bundle_gen)
+
+  return {
+    dom: dom, 
+    dom_errors: dom_errors.entries,
+    main: main, 
+    main_errors: main_errors.entries
+  }
 }
 
